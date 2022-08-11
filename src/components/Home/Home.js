89b263d859cc2d51ad57a5f6aca7485c53ef2a04
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-08-11 11:28:14 +0400

    Removed left padding of logo in navbar

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import './Home.css'; 
+import React from 'react';
+import {HiArrowNarrowRight} from "react-icons/hi";
+
+const Home = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Home;
