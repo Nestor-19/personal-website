7999fb8d1786f6174e8d73a073b22b37b687d041
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-08-14 12:41:46 +0400

    Added typewriter animation and made Home button responsive

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
-import React, { useState , useEffect} from 'react';
+import React from 'react';
 import './Home.css';
 import {HiArrowNarrowRight} from "react-icons/hi"
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
 
@@ -10,11 +11,18 @@ const Home = () => {
 
                 <p className='text-[#ccd6f6] text-2xl'>Hey, I'm</p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ffc857]'>Nestor Dsouza, </h1>
-                <h2 className='text-2xl sm:text-3xl font-bold text-[#ccd6f6]'>a software developer</h2>
-
+                <h2 className='text-2xl sm:text-3xl font-bold text-[#ccd6f6]'>
+                    <Typewriter
+                        onInit={(typewriter) => {
+                            typewriter.typeString("a software developer.")
+                            .start();
+                        }}
+                    />
+                </h2>
                 
+
                 <div className='flex items-center'>
-                    <a href="#about" class="relative inline-block px-2 py-3 my-8 font-medium group">
+                    <a href="#about" class="relative inline-block px-2 py-3 my-8 font-medium group text-sm sm:text-base">
                         <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
                         <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
                         <span class="relative text-black group-hover:text-white flex items-center mx-3 gap-x-2.5 animate-pulse">Check out my page
