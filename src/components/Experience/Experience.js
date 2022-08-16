dac60f69e6c881b46f263b558f6a6230c92d883e
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-08-16 20:36:01 +0400

    Updated Experience section with new layout

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -12,15 +12,10 @@ const Experience = () => {
                     </p>
                 </div>
 
-                <div className='grid grid-cols-2 pl-4 pr-4 text-gray-200 gap-[1%]'>
-                    <div className='pt-[50%] sm:pt-20'>
-                        <ul className=" w-[50%] sm:w-[40%] h-[18%] sm:h-[25%] text-sm font-bold rounded-lg border bg-[#1f2041]">
-                            <li className="py-2 px-4 rounded-t-lg text-white">Metadata Technologies</li>
-                        </ul>
-                    </div>
+                <div className='pl-4 pr-4 text-gray-200 gap-[1%]'>
     
                     <div className='rounded-lg border flex flex-col gap-4 bg-[#1f2041] pt-2 pl-2 pb-2'>
-                        <p className='font-bold text-white'>CRM Analyst @ <p className='inline text-[#ffc857]'>Metadata</p></p>
+                        <p className='font-bold text-white'>CRM Analyst @ <p className='inline text-[#ffc857]'>Metadata Technologies</p></p>
                         <p>July 2019 – Sept. 2019</p>
                         <div className=' flex flex-col gap-2'>
                             <li>Used Microsoft Dynamics CRM to maintain company database with essential account and sales information.</li>
@@ -31,7 +26,7 @@ const Experience = () => {
 
                 </div>
             </div>
-            <div className='pt-[11%] animate-bounce text-[150%] pr-[5%] text-white'>
+            <div className='pt-[11%] animate-bounce text-[150%] pr-[3%] text-white'>
                 <button><AiFillCaretDown /></button>
             </div>
         </div>
