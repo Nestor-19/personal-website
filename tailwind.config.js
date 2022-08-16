a639c0e24d720599fe0ed1277ae108c45876ca0b
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-08-16 17:24:48 +0400

    Removed flowbite package from project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}", "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
+    "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [],
 }
