26347280dea829460769ac204ccbc153e9d16b06
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-08-16 21:15:45 +0400

    Set up Projects Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigationbar from "./components/Navbar/Navbar"
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Experience from './components/Experience/Experience';
+import Projects from './components/Projects/Projects';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Home />
       <About />
       <Experience />
+      <Projects />
     </div>
     
   );
