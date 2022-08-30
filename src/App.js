1d7003ddc0f331eb658839a11819b6e8a8b1e615
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-08-30 18:21:35 +0400

    Created Contact Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
 import Navigationbar from "./components/Navbar/Navbar"
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Experience from './components/Experience/Experience';
 import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
 
 
 function App() {
   return (
     <div>
-      {/* <Routes>
-        <Route path="/" element={<Navigationbar />} />
-        <Route path="/home" element={<Home />} />
-      </Routes> */}
       <Navigationbar />
       <Home />
       <About />
       <Experience />
       <Projects />
+      <Contact />
     </div>
     
   );
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,19 @@
+
+
+const Contact = () => {
+  return (
+    <div name='Contact' className="w-full h-screen flex items-center justify-center sm:mt-[15%] p-4">
+        <form action='' className="flex flex-col max-w-[1000px] w-full">
+            <div className="pb-8 text-gray-300">
+                <p className="text-4xl font-bold inline hover:text-[#ffc857]">👋 Contact Me</p>
+                <p className="pt-5 text-2xl">Say Hi!</p>
+            </div>
+            <input className='bg-[#ccd6f6] p-2 rounded-md outline-none placeholder-black placeholder-opacity-75' type='text' placeholder="Name" name='name'></input>
+            <input className='my-4 p-2 bg-[#ccd6f6] rounded-md outline-none placeholder-black placeholder-opacity-75' type='email' placeholder="Email" name='email'></input>
+            <textarea className='my-4 bg-[#ccd6f6] p-2 rounded-md outline-none placeholder-black placeholder-opacity-75' name='message' rows='10' placeholder="Message me!"></textarea>
+        </form>
+    </div>
+  )
+}
+
+export default Contact
