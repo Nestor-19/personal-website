d9aa02027e93ddc21e0ae0548bcfb01db8dfe48f
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-09-01 13:16:13 +0400

    Created Footer component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About/About';
 import Experience from './components/Experience/Experience';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Experience />
       <Projects />
       <Contact />
+      <Footer />
     </div>
     
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full h-[50px] flex flex-col justify-center items-center text-[#ccd6f6] text-sm mb-8'>
+        <p>Designed and built by Nestor Dsouza.</p>
+        <p>All rights reserved. ©</p>
+    </div>
+  )
+}
+
+export default Footer
