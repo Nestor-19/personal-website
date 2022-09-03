5512907c1b249f6bfe8d2d14b2526dec8e6e7822
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-09-03 13:16:02 +0400

    Fixed overlay bug with correct z-index

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,7 +13,7 @@ const Navigationbar = () => {
   }
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#16172a] text-[#ccd6f6] font-light'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#16172a] text-[#ccd6f6] font-light z-20 scrollbar'>
       <div>
         <img className='logo' src={logo} alt="logo" />
       </div>
@@ -51,7 +51,7 @@ const Navigationbar = () => {
       </div>
 
       {/* Mobile Menu Layout */}
-      <ul className={!toggle ? 'hidden':'absolute top-0 left-0 right-0 bottom-0 w-full h-screen bg-[#16172a] flex flex-col justify-center items-center'}>
+      <ul className={!toggle ? 'hidden':'absolute top-0 left-0 w-full h-screen bg-[#16172a] flex flex-col justify-center items-center'}>
         <li className='py-6 text-4xl navbar-headings'>
           <Link to="Home" smooth={true} duration={500} onClick={handleClick}><p className='cursor-pointer'>Home</p></Link>
         </li>
